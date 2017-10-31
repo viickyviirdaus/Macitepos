91c4c1031a09c34999c8c65360d0f96ed603467d
DESKTOP-SKAJIBU\Global Store <habridio88@gmail.com>
2017-10-31 11:20:44 +0700

    Dashboard Kasir

---
+++ src/main/java/com/macitepos/macitepos/controller/KasirController.java
@@ -0,0 +1,16 @@
+package com.macitepos.macitepos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/kasir")
+public class KasirController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String dashboardKasir(){
+        return "kasirDashboard";
+    }
+
+}
