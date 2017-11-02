3d89836f45026cc3caadb6f8361b5fffc05197f5
DESKTOP-SKAJIBU\Global Store <habridio88@gmail.com>
2017-11-02 12:50:37 +0700

    template kasir edit profil

--- src/main/java/com/macitepos/macitepos/controller/KasirController.java
+++ src/main/java/com/macitepos/macitepos/controller/KasirController.java
@@ -10,7 +10,7 @@ public class KasirController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String dashboardKasir(){
-        return "kasir/Customer";
+        return "kasir/editProfil";
     }
 
 }
