f0bf043c00a844e46e6ff2207f32c0a138fd59cb
DESKTOP-SKAJIBU\Global Store <habridio88@gmail.com>
2017-11-02 13:55:43 +0700

    hapus resources ndak di pake

--- src/main/java/com/macitepos/macitepos/controller/KasirController.java
+++ src/main/java/com/macitepos/macitepos/controller/KasirController.java
@@ -10,7 +10,7 @@ public class KasirController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String dashboardKasir(){
-        return "kasir/reportProduct";
+        return "manajer/masterManajer";
     }
 
 }
