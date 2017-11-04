49c17913082129368a61d66ca502b6ffcd926231
Unknown <vicky.virdus@gmail.com>
2017-11-04 21:46:46 +0700

    view

--- src/main/java/com/macitepos/macitepos/config/KonfigurasiSecurity.java
+++ src/main/java/com/macitepos/macitepos/config/KonfigurasiSecurity.java
@@ -41,13 +41,16 @@ public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http)throws Exception{
 
         http
-                .authorizeRequests().antMatchers("/assets/**").permitAll();
+                .authorizeRequests().antMatchers("/assets/**").permitAll()
+                ;
 
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/home", "/member").permitAll()
-                .antMatchers("/manajer/**").hasAnyRole("MANAJER")
-                .antMatchers("/kasir/**").hasAnyRole("KASIR")
+                .antMatchers("/m_dashboard", "/m_customer","/m_editProfil","/m_header","/m_orders","/m_product","m_reportPayment","m_reportProduct","m_user")
+                .hasAnyRole("MANAJER")
+                .antMatchers("/c_dashboard", "/c_customer","/c_editProfil","/c_header","/c_orders","/c_product","c_reportPayment","c_reportProduct","c_user")
+                .hasAnyRole("KASIR")
                 .antMatchers("/member/create").hasAnyRole("KASIR","MANAJER")
                 .anyRequest().authenticated()
 
